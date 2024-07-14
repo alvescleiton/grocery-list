c1d95467e9299da31c08ffec88d79e2af390b142
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-14 14:38:52 -0300

    adding a new color on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-75': '#F4F4F4',
+      },
+    },
   },
   plugins: [],
 };
