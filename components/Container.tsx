74c5b22aea4508b25d92cce4f34132e763f9c82f
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-14 04:34:30 -0300

    adjusting container component

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,9 +1,14 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className={styles.container}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView className={styles.container}>
+      <View className={styles.view}>{children}</View>
+    </SafeAreaView>
+  );
 };
 
 const styles = {
-  container: 'flex flex-1 p-6',
+  container: 'flex flex-1 bg-white',
+  view: 'flex flex-1 p-4',
 };
