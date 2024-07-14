0e00d1ae9143735a786da3e01587bbf2956cdda4
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-14 17:34:50 -0300

    removing const styles

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,13 +2,8 @@ import { SafeAreaView, View } from 'react-native';
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SafeAreaView className={styles.container}>
-      <View className={styles.view}>{children}</View>
+    <SafeAreaView className="bg-gray-75 flex flex-1">
+      <View className="flex flex-1 p-4">{children}</View>
     </SafeAreaView>
   );
-};
-
-const styles = {
-  container: 'flex flex-1 bg-white',
-  view: 'flex flex-1 p-4',
 };
