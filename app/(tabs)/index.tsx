f194647dffe35b1864145c3efe73e8993d493e0f
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-19 14:32:46 -0300

    listing products from supabase

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,33 @@
-import { FlatList, ListRenderItem, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, FlatList, ListRenderItem, View } from 'react-native';
 
 import { Container } from '~/components/atoms/container/Container';
 import { HeaderStack } from '~/components/atoms/headerStack/HeaderStack';
 import { ProdutItem } from '~/components/atoms/product/ProductItem';
 import { Search } from '~/components/atoms/search/Search';
-import productList from '~/data/productList.json';
 import { ProductItemType } from '~/types/productItem';
+import { supabase } from '~/utils/supabase';
 
 export default function Home() {
+  const [productList, setProductList] = useState<ProductItemType[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase
+        .from('product_list')
+        .select('*')
+        .order('title', { ascending: true });
+
+      if (error) {
+        Alert.alert('Erro', 'Erro ao retornar a lista de produtos');
+      }
+
+      const list = data?.sort((a, b) => a.title.localeCompare(b.title));
+
+      setProductList(list as ProductItemType[]);
+    })();
+  }, []);
+
   const renderItems: ListRenderItem<ProductItemType> = ({ item }) => {
     return <ProdutItem item={item} />;
   };
