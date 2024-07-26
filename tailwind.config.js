5931607e88ffe1ade0c484f00f294fe37d1d0888
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-26 00:44:53 -0300

    configuring tailwind dynamic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import Colors from './constants/Colors';
+
 module.exports = {
   content: ['./index.{js,ts,tsx}', './app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
-      colors: {
-        'gray-75': '#F4F4F4',
-        primary: '#F7903B',
-      },
+      colors: Colors,
     },
   },
   plugins: [],
