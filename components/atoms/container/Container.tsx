30ed52f0e8cf932dd59195d0e890a626b71fdf52
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-26 00:45:43 -0300

    fixing color name

--- components/atoms/container/Container.tsx
+++ components/atoms/container/Container.tsx
@@ -2,7 +2,7 @@ import { SafeAreaView, View } from 'react-native';
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SafeAreaView className="flex flex-1 bg-gray-75">
+    <SafeAreaView className="bg-grayExtraLight flex flex-1">
       <View className="flex flex-1 p-4">{children}</View>
     </SafeAreaView>
   );
