0b4f439c87b3dc0cc29a4226cd733eb51bbdfaf5
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-07-26 01:38:29 -0300

    adding spinner and validations

--- app/(tabs)/items-to-buy.tsx
+++ app/(tabs)/items-to-buy.tsx
@@ -1,5 +1,14 @@
 import { useIsFocused } from '@react-navigation/native';
-import { Alert, FlatList, ListRenderItem, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  ListRenderItem,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import { Container } from '~/components/atoms/container/Container';
 import { HeaderStack } from '~/components/atoms/headerStack/HeaderStack';
@@ -11,12 +20,58 @@ import useItemsToBuy from '~/hooks/useItemsToBuy';
 import { ItemsToBuy } from '~/types/itemsToBuy';
 
 export default function ItemsToBuyPage() {
+  const [loading, setLoading] = useState<boolean>(false);
   const pageLoaded = useIsFocused();
   const { isLoading, error, data: productList } = useItemsToBuy(pageLoaded);
+
+  useEffect(() => {
+    setLoading(isLoading);
+  }, [isLoading]);
 
   if (error) {
     Alert.alert('Erro', error);
   }
+
+  const handleCompleteOrder = () => {
+    const allChecked = _allProductsChecked();
+    _showCompleteOrderAlert(allChecked);
+  };
+
+  const _allProductsChecked = () => {
+    const allChecked = productList.every((item) => !!item.checked);
+
+    return allChecked;
+  };
+
+  const _showCompleteOrderAlert = (allChecked: boolean) => {
+    let title = 'Finalizar Pedido';
+    let message = 'Deseja realmente finalizar o pedido?';
+
+    if (!allChecked) {
+      title = 'ATENÇÃO';
+      message = 'Há itens que não foram marcados. Deseja mesmo finalizar o pedido?';
+    }
+
+    Alert.alert(title, message, [
+      {
+        text: 'Cancelar',
+        style: 'cancel',
+      },
+      {
+        text: 'OK',
+        onPress: () => completeOrder(),
+      },
+    ]);
+  };
+
+  const completeOrder = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      Alert.alert('Success', 'Finalizar Pedido');
+      setLoading(false);
+    }, 3000);
+  };
 
   const renderItems: ListRenderItem<ItemsToBuy> = ({ item }) => {
     return (
@@ -33,25 +88,32 @@ export default function ItemsToBuyPage() {
         {!isLoading && !productList.length && <NoItem />}
 
         {!isLoading ? (
-          <View>
-            <FlatList
-              data={productList}
-              renderItem={renderItems}
-              showsVerticalScrollIndicator={false}
-              contentContainerClassName="pt-4 pb-14"
-            />
-          </View>
+          <>
+            <View>
+              <FlatList
+                data={productList}
+                renderItem={renderItems}
+                showsVerticalScrollIndicator={false}
+                contentContainerClassName="pt-4 pb-14"
+              />
+            </View>
+
+            <View className="absolute bottom-2 left-4 w-full">
+              <TouchableOpacity
+                activeOpacity={0.7}
+                className={`flex h-14 w-full items-center justify-center rounded-md ${loading ? 'bg-grayMedium' : 'bg-greenLight'}`}
+                onPress={() => (loading ? {} : handleCompleteOrder())}>
+                {!loading ? (
+                  <Text className="text-lg font-bold text-white">Finalizar Pedido</Text>
+                ) : (
+                  <ActivityIndicator color="white" size="small" />
+                )}
+              </TouchableOpacity>
+            </View>
+          </>
         ) : (
           <ProductItemSkeleton qtd={10} />
         )}
-
-        <View className="absolute bottom-2 left-4 w-full">
-          <TouchableOpacity
-            className="bg-greenLight flex h-14 w-full items-center justify-center rounded-md"
-            onPress={() => {}}>
-            <Text className="text-lg font-bold text-white">Finalizar Pedido</Text>
-          </TouchableOpacity>
-        </View>
       </Container>
     </>
   );
